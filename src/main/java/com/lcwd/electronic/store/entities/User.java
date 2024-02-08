36a31e6eb242405121972fe71000c9e40df32bae
saurabh kumar <saurabhaggarwal2017@gmail.com>
2024-02-08 22:11:07 +0530

    make the entite User and Repo for User using JpaRepository and user Dto file for transfer data controller to service layer and vice versa

---
+++ src/main/java/com/lcwd/electronic/store/entities/User.java
@@ -0,0 +1,37 @@
+package com.lcwd.electronic.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private String userId;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_email", unique = true)
+    private String email;
+
+    @Column(name = "user_password", length = 15)
+    private String password;
+
+    @Column(name = "user_gender")
+    private String gender;
+
+    @Column(name = "user_about", length = 1000)
+    private String about;
+
+    @Column(name = "user_image_name")
+    private String imageName;
+}
