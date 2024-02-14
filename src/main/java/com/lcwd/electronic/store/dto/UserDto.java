68025262cf5c8efd6948f571103e4db9738d863a
saurabh kumar <saurabhaggarwal2017@gmail.com>
2024-02-14 14:16:36 +0530

    Email validation not working perfect so i used pattern

--- src/main/java/com/lcwd/electronic/store/dto/UserDto.java
+++ src/main/java/com/lcwd/electronic/store/dto/UserDto.java
@@ -2,6 +2,7 @@ package com.lcwd.electronic.store.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -17,8 +18,12 @@ public class UserDto {
 
     @Size(min = 3, max = 30, message = "Invalid Name!!")
     private String name;
-    @Email(message = "Invalid email!!")
+
+    //    @Email(message = "Invalid email!!")
+    @Pattern(regexp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$" , message = "Invalid email!")
     private String email;
+
     @NotBlank(message = "password is required!!")
     private String password;
     @Size(min = 4, max = 6, message = "Invalid gender!!")
